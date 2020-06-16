d439d21637559e1e8f8165b1b6a7c02832e8f305
AnaSofiaDelaCruz <anasofia_navarro@hotmail.com>
2020-06-16 12:02:56 -0500

    static heroku

--- CS/settings.py
+++ CS/settings.py
@@ -69,9 +69,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'CS.urls'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 TEMPLATES = [
     {
